e52710d51ca521f47a5238323b9d383ee9991300
Omri Shaiko <omri@shaiko.com>
2015-09-21 09:35:16 +0300

    Implement request to SE

--- app/app.js
+++ app/app.js
@@ -5,16 +5,46 @@ var app = angular.module('stackD3', []);
 app.controller('UsersCtrl', function ($scope, StackApiResource) {
 
   $scope.searchUser = function (userName) {
-    var x = StackApiResource.getUserBasicInfo(userName);
-    console.log(x);
+    StackApiResource.getUserBasicInfo(userName)
+      .then(function (res) {
+        $scope.user = res.data;
+      })
+      .catch(function () {
+        alert('Something wrong happen...');
+      });
+
   }
 
 });
 
-app.factory('StackApiResource', function () {
+app.factory('StackApiResource', function ($http) {
+
+  //Const variables
+  var BASE_URL = 'https://api.stackexchange.com/2.2/';
+  var BASE_REQUEST_PARAMS = {
+    key: '74Hxi3QyTcBisWml7ou5)g((',
+    site: 'stackoverflow',
+    order: 'desc',
+    sort: 'reputation',
+    filter: 'default'
+  };
+
+  //Will turn an object into query string
+  function serialize(obj) {
+    var str = [];
+    for (var p in obj) {
+      if (obj.hasOwnProperty(p)) {
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+      }
+    }
+    return str.join("&");
+  }
+
   return {
-    getUserBasicInfo : function (userName) {
-      return {a:'b'}
+    getUserBasicInfo: function (userName) {
+      BASE_REQUEST_PARAMS.inname = userName;
+      var url = BASE_URL + 'users?' + serialize(BASE_REQUEST_PARAMS);
+      return $http.get(url);
     }
   }
 });
