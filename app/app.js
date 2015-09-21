0683c4ec45310751d69cf45561da95f6baa9b5b7
Omri Shaiko <omri@shaiko.com>
2015-09-21 15:57:35 +0300

    Added lo dash Map badge object for D3

--- app/app.js
+++ app/app.js
@@ -11,12 +11,12 @@ app.controller('UsersCtrl', function ($scope, StackApiResource) {
         //Could be many users, i'm taking only the first here.
         $scope.user = res.data.items[0];
 
-        var data = [
-          {name: 'bronze', value: 33},
-          {name: 'silver', value: 10},
-          {name: 'gold', value: 2}
-        ]
+        //Map data for D3
+        var data = _.map(res.data.items[0].badge_counts, function(item, key){
+          return {name: key, value: item}
+        });
 
+        //Build the chart based on the data
         buildChart('d3-container',data);
       })
       .catch(function () {
