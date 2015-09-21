c70500c28d47252084c7173bfbd3c5797ae9fc0f
Omri Shaiko <omri@shaiko.com>
2015-09-21 17:21:21 +0300

    Format text

--- app/app.js
+++ app/app.js
@@ -13,15 +13,15 @@ app.controller('UsersCtrl', function ($scope, StackApiResource) {
         $scope.user = res.data.items[0];
 
         //Map data for D3
-        var data = _.map(res.data.items[0].badge_counts, function(item, key){
+        var data = _.map(res.data.items[0].badge_counts, function (item, key) {
           return {name: key, value: item}
         });
 
         //Build the chart based on the data
-        buildChart('d3-container',data);
+        buildChart('d3-container', data);
       })
       .catch(function () {
         $scope.error = true;
       });
-  }
+  };
 });
